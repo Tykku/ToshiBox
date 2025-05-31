2a99e596aefdbe82c0bc5ddded304e077e8b3369
Tykku <selfappointedolive@gmail.com>
2025-05-31 06:55:15 -0500

    UI Fixin and retainer terminatin

--- ToshiBox/Common/Helpers.cs
+++ ToshiBox/Common/Helpers.cs
@@ -14,7 +14,7 @@ public static class Helpers
     public static void PrintToshi(string message, byte? messageColor = null)
     {
         var ssb = new SeStringBuilder();
-        ssb.AddUiForeground("[ToshiBox]", 34); // colored prefix
+        ssb.AddUiForeground($"[{nameof(ToshiBox)}]", 48); // colored prefix
         ssb.AddText(" ");                       // space separator
 
         if (messageColor.HasValue)
@@ -25,82 +25,17 @@ public static class Helpers
         Svc.Chat.Print(ssb.Build());
     }
     
-    /// <summary> Iterate over enumerables with additional index. </summary>
-    public static IEnumerable<(T Value, int Index)> WithIndex<T>(this IEnumerable<T> list)
-        => list.Select((x, i) => (x, i));
-
-    /// <summary> Remove an added index from an indexed enumerable. </summary>
-    public static IEnumerable<T> WithoutIndex<T>(this IEnumerable<(T Value, int Index)> list)
-        => list.Select(x => x.Value);
-
-    /// <summary> Remove the value and only keep the index from an indexed enumerable. </summary>
-    public static IEnumerable<int> WithoutValue<T>(this IEnumerable<(T Value, int Index)> list)
-        => list.Select(x => x.Index);
-
-
-    // Find the index of the first object fulfilling predicate's criteria in the given list.
-    // Returns -1 if no such object is found.
-    public static int IndexOf<T>(this IEnumerable<T> array, Predicate<T> predicate)
+    public static void PrintGlowshi(string message, byte? messageColor = null)
     {
-        var i = 0;
-        foreach (var obj in array)
-        {
-            if (predicate(obj))
-                return i;
+        var ssb = new SeStringBuilder();
+        ssb.AddUiGlow($"[{nameof(ToshiBox)}]", 48); // colored prefix
+        ssb.AddText(" ");                       // space separator
 
-            ++i;
-        }
+        if (messageColor.HasValue)
+            ssb.AddUiForeground(message, messageColor.Value);
+        else
+            ssb.AddText(message);
 
-        return -1;
-    }
-
-    // Find the index of the first occurrence of needle in the given list.
-    // Returns -1 if needle is not contained in the list.
-    public static int IndexOf<T>(this IEnumerable<T> array, T needle) where T : notnull
-    {
-        var i = 0;
-        foreach (var obj in array)
-        {
-            if (needle.Equals(obj))
-                return i;
-
-            ++i;
-        }
-
-        return -1;
-    }
-
-    // Find the first object fulfilling predicate's criteria in the given list, if one exists.
-    // Returns true if an object is found, false otherwise.
-    public static bool FindFirst<T>(this IEnumerable<T> array, Predicate<T> predicate, [NotNullWhen(true)] out T? result)
-    {
-        foreach (var obj in array)
-        {
-            if (predicate(obj))
-            {
-                result = obj!;
-                return true;
-            }
-        }
-
-        result = default;
-        return false;
-    }
-
-    // Find the first occurrence of needle in the given list and return the value contained in the list in result.
-    // Returns true if an object is found, false otherwise.
-    public static bool FindFirst<T>(this IEnumerable<T> array, T needle, [NotNullWhen(true)] out T? result) where T : notnull
-    {
-        foreach (var obj in array)
-        {
-            if (obj.Equals(needle))
-            {
-                result = obj;
-                return true;
-            }
-        }
-
-        result = default;
-        return false;
+        Svc.Chat.Print(ssb.Build());
     }
 }
